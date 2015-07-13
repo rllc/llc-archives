3f7a483757add2e4ff056f650c154c6e77a555b1
Steven McAdams <smcadams86@gmail.com>
2015-07-13 18:56:30 -0500

    commenting out bootstrap

--- src/main/groovy/com/rllc/spreadsheet/Application.groovy
+++ src/main/groovy/com/rllc/spreadsheet/Application.groovy
@@ -1,15 +1,14 @@
 package com.rllc.spreadsheet
 
-import com.rllc.spreadsheet.config.Bootstrap
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.ApplicationContext
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        ctx.getBean(Bootstrap.class).execute()
+        SpringApplication.run(Application.class, args);
+//        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+//        ctx.getBean(Bootstrap.class).execute()
     }
 }
