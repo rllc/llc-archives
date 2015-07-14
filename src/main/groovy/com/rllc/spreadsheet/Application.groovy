79468c76efa11125075256c5d539c7fd38a1803a
Steven McAdams <smcadams86@gmail.com>
2015-07-13 19:22:11 -0500

    debug logging for gradle stage

--- src/main/groovy/com/rllc/spreadsheet/Application.groovy
+++ src/main/groovy/com/rllc/spreadsheet/Application.groovy
@@ -1,14 +1,15 @@
 package com.rllc.spreadsheet
 
+import com.rllc.spreadsheet.config.Bootstrap
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.ApplicationContext
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-//        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-//        ctx.getBean(Bootstrap.class).execute()
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        ctx.getBean(Bootstrap.class).execute()
     }
 }
