06fdcdd32a818b464b01d34a30d02d3cc3a37aa8
Steven McAdams <smcadams86@gmail.com>
2015-07-14 00:09:35 -0500

    log when loading datasource

--- src/main/groovy/com/rllc/spreadsheet/config/CoreConfiguration.groovy
+++ src/main/groovy/com/rllc/spreadsheet/config/CoreConfiguration.groovy
@@ -1,6 +1,8 @@
 package com.rllc.spreadsheet.config
 
 import org.apache.commons.dbcp2.BasicDataSource
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
@@ -15,8 +17,11 @@ import javax.sql.DataSource
 @PropertySource("classpath:spreadsheet.properties")
 public class CoreConfiguration {
 
+    private static final Logger logger = LoggerFactory.getLogger(CoreConfiguration.class);
+
     @Bean
     public DataSource dataSource() {
+        logger.info "loading datasource..."
 
         URI dbUri;
         try {
@@ -24,6 +29,8 @@ public class CoreConfiguration {
             String password = "password";
             String url = "jdbc:postgresql://localhost/llc";
             String dbProperty = System.getProperty("DATABASE_URL");
+            logger.info "dbProperty : $dbProperty"
+
 
             if (dbProperty != null) {
                 dbUri = new URI(dbProperty);
