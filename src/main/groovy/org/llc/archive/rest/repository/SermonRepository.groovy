20ea8190ce930f4368a87083f3c9aff9d2722c04
Steven McAdams <smcadams86@gmail.com>
2015-11-12 17:33:50 -0600

    refactoring sermons to be served via a single page

--- src/main/groovy/org/llc/archive/rest/repository/SermonRepository.groovy
+++ src/main/groovy/org/llc/archive/rest/repository/SermonRepository.groovy
@@ -1,6 +1,9 @@
 package org.llc.archive.rest.repository
 
 import org.llc.archive.rest.domain.Sermon
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
@@ -13,7 +16,30 @@ interface SermonRepository extends PagingAndSortingRepository<Sermon, Long> {
 
     List<Sermon> findById(@Param("id") long id);
 
-    List<Sermon> findByCongregation_Name(@Param("name") String name);
+    Page<Sermon> findByCongregation_Name(@Param("name") String name, Pageable pageable)
+
+    @Query(value = """
+        select s from Sermon s
+        where
+          s.congregation.name = :name and
+          (
+            LOWER(s.minister) like %:query% or
+            LOWER(s.bibleText) like %:query% or
+            LOWER(s.comments) like %:query%
+          )
+    """)
+    Page<Sermon> findAnyFieldMatchingQueryByCongregation(
+            @Param("name") String name, @Param("query") query, Pageable pageable)
 
     List<Sermon> findByFileUrlEndingWith(@Param("file") String file);
+
+    @Query(value = """
+        select s from Sermon s
+        where
+          LOWER(s.minister) like %:query% or
+          LOWER(s.bibleText) like %:query% or
+          LOWER(s.comments) like %:query%
+    """)
+    Page<Sermon> findAnyFieldMatchingQuery(@Param("query") String query, Pageable pageable);
+
 }
--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -10,18 +10,14 @@
  */
 angular
     .module('llcArchivesApp', [
-        'ngAnimate',
-        'ngResource',
         'ngRoute',
-        'ngSanitize',
-        'ngTouch',
         'spring-data-rest'
     ])
     .config(function ($routeProvider) {
         $routeProvider
             .when('/', {
-                templateUrl: 'views/main.html',
-                controller: 'MainCtrl'
+                templateUrl: 'views/sermon.html',
+                controller: 'SermonCtrl'
             })
             .when('/congregations/:name', {
                 templateUrl: 'views/sermon.html',
