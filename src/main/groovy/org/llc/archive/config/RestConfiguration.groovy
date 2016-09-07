cc4ca33687b7a98a22fd1e28d1519ade6104720b
Steven McAdams <smcadams86@gmail.com>
2016-09-06 22:44:18 -0500

    spring rest configuration to RepositoryRestConfigurerAdapter

--- src/main/groovy/org/llc/archive/config/RestConfiguration.groovy
+++ src/main/groovy/org/llc/archive/config/RestConfiguration.groovy
@@ -2,19 +2,19 @@ package org.llc.archive.config
 
 import org.llc.archive.rest.domain.Congregation
 import org.llc.archive.rest.domain.Sermon
-import org.springframework.boot.autoconfigure.data.rest.SpringBootRepositoryRestMvcConfiguration
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter
 import org.springframework.http.MediaType
 
 /**
  * Created by Steven McAdams on 6/11/15.
  */
 @Configuration
-public class RestConfiguration extends SpringBootRepositoryRestMvcConfiguration {
+public class RestConfiguration extends RepositoryRestConfigurerAdapter {
 
     @Override
-    protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.defaultMediaType = MediaType.APPLICATION_JSON
         config.exposeIdsFor(Sermon.class);
         config.exposeIdsFor(Congregation.class);
